0c6535c9f4962e820687287b20ef306a9fb78b93
pelinyavuz <46028541+pelinyavuz@users.noreply.github.com>
2019-04-04 14:17:48 +0300

    deneme

---
+++ TezControlSystem/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TezControlSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorize = new System.Windows.Forms.Button();
+            this.deleteedges = new System.Windows.Forms.Button();
+            this.nextstep = new System.Windows.Forms.Button();
+            this.socketadd = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorize
+            // 
+            this.colorize.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.colorize.Location = new System.Drawing.Point(289, 416);
+            this.colorize.Margin = new System.Windows.Forms.Padding(2);
+            this.colorize.Name = "colorize";
+            this.colorize.Size = new System.Drawing.Size(91, 46);
+            this.colorize.TabIndex = 14;
+            this.colorize.Text = "Colorize";
+            this.colorize.UseVisualStyleBackColor = true;
+            this.colorize.Click += new System.EventHandler(this.colorize_Click);
+            // 
+            // deleteedges
+            // 
+            this.deleteedges.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.deleteedges.Location = new System.Drawing.Point(128, 416);
+            this.deleteedges.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteedges.Name = "deleteedges";
+            this.deleteedges.Size = new System.Drawing.Size(91, 46);
+            this.deleteedges.TabIndex = 13;
+            this.deleteedges.Text = "Delete Edges";
+            this.deleteedges.UseVisualStyleBackColor = true;
+            this.deleteedges.Click += new System.EventHandler(this.deleteedges_Click);
+            // 
+            // nextstep
+            // 
+            this.nextstep.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.nextstep.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.nextstep.Location = new System.Drawing.Point(412, 416);
+            this.nextstep.Margin = new System.Windows.Forms.Padding(2);
+            this.nextstep.Name = "nextstep";
+            this.nextstep.Size = new System.Drawing.Size(91, 46);
+            this.nextstep.TabIndex = 10;
+            this.nextstep.Text = "NextStep";
+            this.nextstep.UseVisualStyleBackColor = false;
+            this.nextstep.Click += new System.EventHandler(this.nextstep_Click);
+            // 
+            // socketadd
+            // 
+            this.socketadd.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.socketadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.socketadd.Location = new System.Drawing.Point(33, 416);
+            this.socketadd.Margin = new System.Windows.Forms.Padding(2);
+            this.socketadd.Name = "socketadd";
+            this.socketadd.Size = new System.Drawing.Size(91, 46);
+            this.socketadd.TabIndex = 9;
+            this.socketadd.Text = "Socket Add";
+            this.socketadd.UseVisualStyleBackColor = false;
+            this.socketadd.Click += new System.EventHandler(this.socketadd_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(470, 406);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 492);
+            this.Controls.Add(this.colorize);
+            this.Controls.Add(this.deleteedges);
+            this.Controls.Add(this.nextstep);
+            this.Controls.Add(this.socketadd);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button colorize;
+        private System.Windows.Forms.Button deleteedges;
+        private System.Windows.Forms.Button nextstep;
+        private System.Windows.Forms.Button socketadd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TezControlSystem/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Imaging.Filters;
+using AForge.Imaging;
+using AForge.Math.Geometry;
+using System.Drawing.Imaging;
+namespace TezControlSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Blob[] blobs;
+        int sayac;
+        int toplamSekilSayisi;
+        Bitmap kaynakResim;
+        Bitmap siyahBeyazResim;
+
+        private void socketadd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog rs = new OpenFileDialog();
+            rs.Title = "Resim Seç";
+            rs.Filter = "(*.jpg)|*.jpg|(*.png)|*.png";
+
+            if (rs.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox1.Image = new Bitmap(rs.OpenFile());
+            }
+
+            sayac = 1;
+            Bitmap kaynakResim = new Bitmap(pictureBox1.Image);
+
+            //Otsu Threshold uygulandı
+            OtsuThreshold otsuFiltre = new OtsuThreshold();
+            //resim eğer renkliyse önce griye çeviriyor sonra filtre uyguluyor
+            //resim zaten griyse direk filtreyi uyguluyor
+            siyahBeyazResim = otsuFiltre.Apply(kaynakResim.PixelFormat != PixelFormat.Format8bppIndexed ? Grayscale.CommonAlgorithms.BT709.Apply(kaynakResim) : kaynakResim);
+
+            BlobCounterBase bc = new BlobCounter();
+            bc.FilterBlobs = true;
+            bc.MinHeight = 5;
+            bc.MinWidth = 5;
+
+            bc.ProcessImage(siyahBeyazResim);
+            blobs = bc.GetObjectsInformation();
+
+            for (int i = 0, n = blobs.Length; i < n; i++)
+            {
+                if (blobs.Length > 0)
+                {
+
+                    bc.ExtractBlobsImage(siyahBeyazResim, blobs[i], true);
+                    pictureBox1.Image = siyahBeyazResim;
+                    pictureBox1.Refresh();
+                }
+
+            }
+        }
+
+        private void deleteedges_Click(object sender, EventArgs e)
+        {
+            //filtre oluşturuldu
+            ExtractBiggestBlob filtre = new ExtractBiggestBlob();
+            //filtre uygulandı
+            Bitmap temizResim = filtre.Apply((Bitmap)pictureBox1.Image);
+            pictureBox1.Image = temizResim;
+        }
+
+        private void colorize_Click(object sender, EventArgs e)
+        {
+            //filtre oluşturuldu
+            ConnectedComponentsLabeling filtre = new ConnectedComponentsLabeling();
+            Bitmap boyaliResim = filtre.Apply((Bitmap)pictureBox1.Image);
+            pictureBox1.Image = boyaliResim;
+        }
+
+        private void nextstep_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = blobs[sayac - 1].Image.ToManagedImage();
+
+            if (sayac == toplamSekilSayisi)
+            {
+                sayac = 0;
+            }
+            sayac++;
+        }
+    }
+
+}
+    
